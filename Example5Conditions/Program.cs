bd24091456fc2f2532af6cdb18a41e5f193458b3
nobilitas <ek.peregudova@gmail.com>
2022-12-05 17:09:00 +0300

    If else conditions added Example 5

---
+++ Example5Conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "mary")
+{
+    Console.WriteLine("Great! It's Mary!");
+    }
+else
+{
+    Console.Write("Hello,");
+    Console.Write(username);
+}
