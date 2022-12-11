7e6bcf3cd68c2247d17306567a213a120722866e
nobilitas <ek.peregudova@gmail.com>
2022-12-11 21:13:29 +0300

    Lecture 2, Examples 8,9,10,11 adday Array and Methods

---
+++ Example10ArrayMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 34, 23, 45, 56, 76, 12, 34};
+
+int n = array.Length;
+int find = 34;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
